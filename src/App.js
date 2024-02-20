39a8d4f88ada221ba8cce1e57147126703f80906
LAPTOP-L74LB9B0\TM <tamanash1997@gmail.com>
2024-02-20 20:27:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello App</h1>
+    </div>
+  );
+}
+
+export default App;
